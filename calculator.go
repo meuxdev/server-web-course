07b82524c175d4e5d1a05cfe22e498624f2bed43
Alejandro Andrade <meuxdev@gmail.com>
2022-06-04 17:00:08 -0500

    Handle atoi error

--- calculator.go
+++ calculator.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -15,7 +16,12 @@ func main() {
 	operation := scanner.Text()
 	values := strings.Split(operation, "+")
 	fmt.Println(values)
-	number1, _ := strconv.Atoi(values[0])
-	number2, _ := strconv.Atoi(values[1])
+	number1, err1 := strconv.Atoi(values[0])
+	number2, err2 := strconv.Atoi(values[1])
+
+	if err1 != nil || err2 != nil {
+		log.Fatalln(err1)
+	}
+
 	fmt.Println(number1 + number2)
 }
