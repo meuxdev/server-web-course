ba3842cdfbbcaed682c75b3fb900484494532856
Alejandro Andrade <meuxdev@gmail.com>
2022-06-13 18:22:13 -0500

    Adding the add and delete function to the tasklist

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+type TaskList struct {
+	tasks []*Task
+}
+
+func NewTaskList() *TaskList {
+	return &TaskList{}
+}
+
+func (tl *TaskList) AddTaskToList(task *Task) {
+	tl.tasks = append(tl.tasks, task)
+}
+
+func (tl *TaskList) RemoveTaskFromIndex(index int) {
+	tl.tasks = append(tl.tasks[:index], tl.tasks[index+1:]...)
+
+}
+
+func (tl *TaskList) PrintAllTasks() (chain string) {
+	chain += "-----------------\nTasks List \n------------------\n---------------\n"
+	for _, task := range tl.tasks {
+		chain += task.String()
+	}
+	fmt.Println(chain)
+	return
+}
+
 type Task struct {
 	name        string
 	description string
@@ -33,7 +59,19 @@ func (t *Task) UpdateName(newName string) {
 }
 
 func main() {
+
+	l := NewTaskList()
 	t := NewTask("Task Name", "Some random description to this task.", false)
+
+	// adding 3 tasks
+
+	for i := 0; i < 3; i++ {
+		taskName := fmt.Sprintf("Task Number %d", i+1)
+		tempTask := NewTask(taskName, "Some random description to this task.", false)
+		l.AddTaskToList(tempTask)
+	}
+
+	l.AddTaskToList(t)
 
 	fmt.Printf(t.String())
 	fmt.Println("----")
@@ -42,4 +80,9 @@ func main() {
 	t.UpdateDescription("Complete the go course as soon as possible")
 	t.MarkCompleted()
 	fmt.Printf(t.String())
+
+	l.PrintAllTasks()
+	l.RemoveTaskFromIndex(len(l.tasks) - 1)
+	l.PrintAllTasks()
+
 }
