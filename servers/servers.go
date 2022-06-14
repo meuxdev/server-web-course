1f69f08161fa49ab248e604101308ff29c25a9d5
Alejandro Andrade <meuxdev@gmail.com>
2022-06-14 15:32:54 -0500

    Adding course version with goroutines and channels

--- servers/servers.go
+++ servers/servers.go
@@ -9,6 +9,7 @@ import (
 func main() {
 
 	initialTime := time.Now()
+	c := make(chan string)
 
 	servers := []string{
 		"https://platzi.com",
@@ -18,20 +19,22 @@ func main() {
 	}
 
 	for _, serverUrl := range servers {
-		testServer(serverUrl)
+		go testServer(serverUrl, c)
 	}
 
+	for i := 0; i < len(servers); i++ {
+		fmt.Println(<-c)
+	}
 	timePassed := time.Since(initialTime)
 	fmt.Printf("---------------------------\nExc. Time was %s\n", timePassed)
 }
 
-func testServer(serverURL string) {
+func testServer(serverURL string, c chan<- string) {
 	_, err := http.Get(serverURL)
-	fmt.Printf("Testing Server %s\n---------\n", serverURL)
 
 	if err != nil {
-		fmt.Println("Server no available. 😭")
+		c <- serverURL + " ===  Server NOT working. ❌"
 	} else {
-		fmt.Println("Server working correctly. ✔️")
+		c <- serverURL + " ===  Server working. ✔️"
 	}
 }
