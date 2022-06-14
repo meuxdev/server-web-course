781a900cb2f41e8d174a38473838c7a7c1e1ab58
Alejandro Andrade <meuxdev@gmail.com>
2022-06-14 15:20:52 -0500

    Adding my version of goroutines

--- servers/servers.go
+++ servers/servers.go
@@ -3,35 +3,47 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
 func main() {
 
 	initialTime := time.Now()
-
+	var wg sync.WaitGroup
 	servers := []string{
 		"https://platzi.com",
 		"https://google.com",
 		"https://facebook.com",
 		"https://instagram.com",
 	}
+	workersAmount := len(servers)
+	c := make(chan string, workersAmount)
 
 	for _, serverUrl := range servers {
-		testServer(serverUrl)
+		wg.Add(1)
+		//testServer(serverUrl, c, &wg)
+		go testServer(serverUrl, c, &wg)
 	}
 
 	timePassed := time.Since(initialTime)
 	fmt.Printf("---------------------------\nExc. Time was %s\n", timePassed)
+	wg.Wait()
+
 }
 
-func testServer(serverURL string) {
+func testServer(serverURL string, c chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	_, err := http.Get(serverURL)
 	fmt.Printf("Testing Server %s\n---------\n", serverURL)
 
 	if err != nil {
 		fmt.Println("Server no available. 😭")
+		c <- ""
+
 	} else {
 		fmt.Println("Server working correctly. ✔️")
+		c <- "OK"
 	}
+
 }
