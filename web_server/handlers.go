8cca2fa28c3937fa97c4f045f322bb7d277de283
Alejandro Andrade <meuxdev@gmail.com>
2022-06-21 23:55:28 -0500

    Handle the Models and the JSON decoder

--- web_server/handlers.go
+++ web_server/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -11,4 +12,19 @@ func HandleRoot(w http.ResponseWriter, req *http.Request) {
 
 func HandleHome(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "This is the API endpoint")
+}
+
+func PostRequest(w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var metadata MetaData
+	// send decoder as reference
+	err := decoder.Decode(&metadata)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+
+	// !Pending error -> fmt.Printf("Name: ", metadata["Name"])
+	// %v so that the message is formated
+	fmt.Fprintf(w, "Payload: %v", metadata)
 }
--- web_server/types.go
+++ web_server/types.go
@@ -3,3 +3,12 @@ package main
 import "net/http"
 
 type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+type User struct {
+	Name  string
+	Email string
+	Phone string
+}
+
+type MetaData interface {
+}
