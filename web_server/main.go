20174a1f5fe1e3173053e1da90d5c42f02356227
Alejandro Andrade <meuxdev@gmail.com>
2022-06-23 02:55:33 -0500

    Adding middleware to the user post endpoint.

--- web_server/main.go
+++ web_server/main.go
@@ -2,9 +2,9 @@ package main
 
 func main() {
 	server := NewServer(":8000")
-	server.Handle("GET", "/", server.AddMiddleware(HandleRoot, Logging()))
+	server.Handle("GET", "/", server.AddMiddleware(HandleRoot, Logger()))
 	server.Handle("POST", "/api", server.AddMiddleware(HandleHome, CheckAuth(), Logger()))
-	server.Handle("POST", "/api/user", UserPostRequest)
+	server.Handle("POST", "/api/user", server.AddMiddleware(UserPostRequest, Logger()))
 	server.Listen()
 
 }
